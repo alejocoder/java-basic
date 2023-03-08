db8969ae0478b3f28c9551b84f8ee6d4a35714b5
alejandro <alejosamp@gmail.com>
2023-03-07 20:42:55 -0500

    tema-1

---
+++ TEMA-1/src/tema1/tema1.java
@@ -0,0 +1,15 @@
+package tema1;
+
+public class tema1 {
+    public static void main(String[] args) {
+        System.out.println("int");
+        System.out.println("long");
+        System.out.println("float");
+        System.out.println("byte");
+        System.out.println("short");
+        System.out.println("double");
+        System.out.println("boolean");
+        System.out.println("char");
+        System.out.println("string");
+    }
+}
